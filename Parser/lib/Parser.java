592eef06aba8677b20f5cca2f66bed0600e317eb
Matthew Song <matzsong@gmail.com>
2020-12-09 13:55:45 -0500

    Parser

--- Parser/lib/Parser.java
+++ Parser/lib/Parser.java
@@ -1,55 +1,59 @@
 import java.util.*;
+
 public class Parser {
     private ArrayList<Double> numberList;
-    private ArrayList<Character> operationList; 
+    private ArrayList<Character> operationList;
 
-    public Parser(ArrayList<Double> nL, ArrayList<Character> oL){
+    public Parser(ArrayList<Double> nL, ArrayList<Character> oL) {
         numberList = nL;
         operationList = oL;
     }
 
-    public double evaluate(){
+    public double evaluate() {
         Stack<Double> numbers = new Stack<>();
         Stack<Character> operations = new Stack<>();
         int numbersIndex = 0;
         int operationsIndex = 0;
-        for(int i = 0; i < numberList.size() + operationList.size(); i++){
-            if(i % 2 == 0){
+        for (int i = 0; i < numberList.size() + operationList.size(); i++) {
+            if (i % 2 == 0) {
                 numbers.push(numberList.get(numbersIndex));
                 numbersIndex++;
-            }
-            else{
+            } else {
                 char currentOperation = operationList.get(operationsIndex);
-                char previousOperation = operations.peek();
-                if(!operations.empty() && precedence(previousOperation, currentOperation)){
+                while (!operations.empty() && precedence(operations.peek(), currentOperation)) {
                     double b = numbers.pop();
                     double a = numbers.pop();
                     char op = operations.pop();
-                    numbers.push(evaluateOperation(b,a,op));
+                    numbers.push(evaluateOperation(b, a, op));
                 }
                 operations.push(currentOperation);
                 operationsIndex++;
             }
         }
-        while(!operations.empty()){
+        while (!operations.empty()) {
             double b = numbers.pop();
             double a = numbers.pop();
             char op = operations.pop();
-            numbers.push(evaluateOperation(b,a,op));
+            numbers.push(evaluateOperation(b, a, op));
         }
         return numbers.pop();
     }
 
-    private double evaluateOperation(double b, double a, char op){
-        if(op == '+') return a+b;
-        else if(op == '-') return a-b;
-        else if(op == '*') return a*b;
-        else return a/b; 
+    private double evaluateOperation(double b, double a, char op) {
+        if (op == '+')
+            return a + b;
+        else if (op == '-')
+            return a - b;
+        else if (op == '*')
+            return a * b;
+        else
+            return a / b;
     }
 
-    private boolean precedence(char op1, char op2){
-        if((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) return false;
-        return true; 
+    private boolean precedence(char op1, char op2) {
+        if ((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/'))
+            return false;
+        return true;
     }
 
 }
--- Parser/lib/ParserInterface.java
+++ Parser/lib/ParserInterface.java
@@ -14,37 +14,76 @@ public class ParserInterface {
         try {
             if (expression.charAt(0) != '=')
                 throw new NoEqualSignException("Expression doesn't start with equal sign.");
-            
+            while (stringIndex < expression.length()) {
+                if (elements % 2 == 0) {
+                    String num = retrieveNumber(expression, stringIndex);
+                    stringIndex += num.length();
+                    numberList.add(Double.parseDouble(num));
+                } else {
+                    operationList.add(retrieveOperation(expression, stringIndex));
+                    stringIndex++;
+                }
+                elements++;
+            }
+            if (elements % 2 == 0)
+                throw new OperatorEndingException("Expression ends with an operation.");
+            Parser p = new Parser(numberList, operationList);
+            System.out.println(p.evaluate());
 
         } catch (NoEqualSignException e) {
             System.out.println(expression);
             System.out.println(e);
-
-        } catch (InvalidOperatorException e) {
-
         } catch (OperatorEndingException e) {
-
-        } catch (NumberFormatException e){
-
-        } catch (ArithmeticException e){
-            
+            System.out.println("Error: " + e);
+        } catch (ExtraneousCharacterException e) {
+            System.out.println(e);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: User-defined number is not able to be parsed as a double.");
+        } catch (ArithmeticException e) {
+            System.out.println("The statement is evaluated as undefined.");
         }
     }
 
-    class NoEqualSignException extends Exception {
+    static char retrieveOperation(String exp, int stringIndex) throws ExtraneousCharacterException {
+        char op = exp.charAt(stringIndex);
+        if (!(op == '+' || op == '-' || op == '*' || op == '/'))
+            throw new ExtraneousCharacterException("There is an extraneous character in the expresion.");
+        return op;
+    }
+
+    static String retrieveNumber(String exp, int stringIndex) {
+        int beginIndex = stringIndex;
+        String num = "";
+        boolean hasDecimal = false;
+        while (stringIndex < exp.length()) {
+            if (exp.charAt(stringIndex) >= '0' && exp.charAt(stringIndex) <= '9')
+                num += exp.charAt(stringIndex);
+            else if (exp.charAt(stringIndex) == '-' && stringIndex == beginIndex)
+                num += exp.charAt(stringIndex);
+            else if (exp.charAt(stringIndex) == '.' && !hasDecimal) {
+                num += exp.charAt(stringIndex);
+                hasDecimal = true;
+            } else
+                break;
+            stringIndex++;
+        }
+        return num;
+    }
+
+    static class NoEqualSignException extends Exception {
         NoEqualSignException(String s) {
             super(s);
         }
     }
 
-    class InvalidOperatorException extends Exception {
-        InvalidOperatorException(String s) {
+    static class OperatorEndingException extends Exception {
+        OperatorEndingException(String s) {
             super(s);
         }
     }
 
-    class OperatorEndingException extends Exception {
-        OperatorEndingException(String s) {
+    static class ExtraneousCharacterException extends Exception {
+        ExtraneousCharacterException(String s) {
             super(s);
         }
     }
